b2cf95f73e0b0b9422eac188236551213a270cc0
Akanksha Gupta <47183804+Akku032@users.noreply.github.com>
2020-11-23 11:45:21 +0530

    internal conflict management System

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(LogIn)
-admin.site.register(Register)
-admin.site.register(Feedback)
-
--- apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Miniproject1Config(AppConfig):
-    name = 'miniproject1'
